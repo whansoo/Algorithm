10c2657671c9a185a3d22209afdcb62efc72799b
신환수(모두의희망) <ghkstn7523@naver.com>
2023-07-12 00:53:23 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,19 @@
+function solution(n) {
+  let result = [];
+  let divisor = 2;
+  
+  while (n >= 2) {
+    if (n % divisor === 0) {
+      result.push(divisor)
+      n = n / divisor;
+    }
+    
+     else divisor ++;
+    
+  }
+  
+  return [...new Set(result)];
+    // return result
+}
+
+
