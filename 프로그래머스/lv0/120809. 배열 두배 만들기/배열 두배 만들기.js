251b1a9ad71b117964e78a45c2bcfd9b377cf16e
신환수(모두의희망) <ghkstn7523@naver.com>
2023-07-12 03:05:49 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,10 @@
+// function solution(numbers) {
+//    let sum = [];
+//    for(let i = 0; i<numbers.length; i++) {
+//    sum.push(numbers[i] * 2)
+//     }
+//   return sum;
+// }
+function solution(numbers) {
+  return numbers.map((v) => v * 2)
+}
