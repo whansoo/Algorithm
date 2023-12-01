149ecdd9dc749ef3c75682d3eccf7a0e4007d75a
신환수(모두의희망) <ghkstn7523@naver.com>
2023-12-01 23:50:18 +0900

    [level unrated] Title: 중복된 문자 제거, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,13 +1,25 @@
+// function solution(my_string) {
+//     const num = my_string.split('')
+//     const a  = num.filter((item,position) => {
+//     return num.indexOf(item) === position
+//   })
+//    return a.join('')
+// }
+// 2번째 풀이
+// function solution(my_string) {
+//     const num = [...new Set(my_string)]
+//     return num.join('')
+// }
+// 3번째 풀이
 function solution(my_string) {
-    // const num = [...new Set(my_string)]
-    // return num.join('')
-    const num = [...my_string]
-const a  = num.filter((item,position) => {
-    return num.indexOf(item) === position
-  })
-   return a.join('')
+    const arr = [...my_string] //	[ 'p', 'e', 'o', 'p', 'l', 'e' ]
+    const a = {}
+    const result = []
+    for (let element of arr) {
+        if(!a[element]) {
+            result.push(element)
+        }
+        a[element] = element
+    }
+    return result.join('')
 }
-
-//new Set()
-//filter
-//chching
