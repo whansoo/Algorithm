20f38139410d0d251e76e7a7092f86481d981bf5
신환수(모두의희망) <ghkstn7523@naver.com>
2023-12-02 16:19:12 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers, n) {
+    let result = 0
+   for(let i = 0; i < numbers.length; i++) {
+       
+       result += numbers[i]
+       if(result > n) {
+           return result
+       }
+   }
+}
