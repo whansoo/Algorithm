c0acbfaa5fe3e719e61445c81bfb424238842108
신환수(모두의희망) <ghkstn7523@naver.com>
2023-12-05 18:18:49 +0900

    [level unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
@@ -1,26 +1,27 @@
 function solution(arr, n) {
-    // const result = []
-    // for(let i = 0; i < arr.length; i++) {
-    //     if(arr.length % 2 === 1) {
-    //       if(arr.indexOf(arr[i]) % 2 === 0) {
-    //           console.log(arr[i])
-    //           result.push(arr[i] + n)
-    //       } else {
-    //           console.log(arr[i])
-    //           result.push(arr[i])
-    //       }
-    //     } else if(arr.length % 2 === 0)  {
-    //         if(arr.indexOf(arr[i]) % 2 === 1) {
-    //              result.push(arr[i] + n)
-    //         } else {
-    //              result.push(arr[i])
-    //         }
-    //     }
-    // }
-    // return result
-     if(arr.length % 2 === 0) {
-        return arr.map((a, i) => i%2 !== 0 ? a+n : a)
-    } else {
-        return arr.map((a, i) => i%2 === 0 ? a+n : a)
+    const result = []
+    for(let i = 0; i < arr.length; i++) {
+        if(arr.length % 2 === 1) {
+          if(i % 2 === 0) {
+              console.log(arr[i])
+              result.push(arr[i] + n)
+          } else {
+              console.log(arr[i])
+              result.push(arr[i])
+          }
+        } else if(arr.length % 2 === 0)  {
+            if(i % 2 === 1) {
+                 result.push(arr[i] + n)
+            } else {
+                 result.push(arr[i])
+            }
+        }
     }
+    return result
+//      if(arr.length % 2 === 0) {
+//        return arr.map((a, i) => i%2 !== 0 ? a+n : a)
+//     } else {
+//        return arr.map((a, i) => i%2 === 0 ? a+n : a)
+       
+//     }
 }
