83d40c3e52bd00cc2e3e17efee5ef7f6007686fd
신환수(모두의희망) <ghkstn7523@naver.com>
2023-12-12 13:57:51 +0900

    [level unrated] Title: 원하는 문자열 찾기, Time: 0.12 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -0,0 +1,11 @@
+function solution(myString, pat) {
+   const my = myString.toLowerCase()
+   const pastr = pat.toLowerCase()
+   
+   if(my.includes(pastr)) {
+       return 1
+   } else {
+       return 0
+   }
+   console.log(my.includes(pastr))
+}
