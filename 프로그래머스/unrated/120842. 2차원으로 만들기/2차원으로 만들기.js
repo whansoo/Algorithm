97829d5a4d3c022ebef236361a65db0ac40efb3f
신환수(모두의희망) <ghkstn7523@naver.com>
2023-12-14 18:40:30 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.11 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,14 +1,24 @@
 function solution(num_list, n) {
-    const result = [];
-    let tempArray = [];
-    for(let i = 0; i< num_list.length; i++) {
-        const item = num_list[i];
-        tempArray.push(item);
-        if(tempArray.length === n) {
-            result.push(tempArray);
-            tempArray = [];
+    const result = []
+    let arr = []
+    for(let i = 0; i < num_list.length; i++) {
+        arr.push(num_list[i])
+        if(arr.length === n) {
+            result.push(arr)
+            arr = [];
         }
-        
     }
-    return result;
+    return result
+//     const result = [];
+//     let tempArray = [];
+//     for(let i = 0; i< num_list.length; i++) {
+//         const item = num_list[i];
+//         tempArray.push(item);
+//         if(tempArray.length === n) {
+//             result.push(tempArray);
+//             tempArray = [];
+//         }
+        
+//     }
+//     return result;
 }
