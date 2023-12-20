ff35dfd622e82082a6744896aac5da202ab9052e
신환수(모두의희망) <ghkstn7523@naver.com>
2023-12-20 10:09:35 +0900

    [level unrated] Title: 수열과 구간 쿼리 1, Time: 2.64 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -0,0 +1,28 @@
+function solution(arr, queries) {
+    return queries.reduce((acc, cur) => {
+        const [from, to] = cur
+        for(let i = from ; i <= to ; i ++) {
+            acc[i]+=1
+        }
+        return acc
+    }, arr)
+}
+
+// function solution(arr, queries) {
+//     let result = []
+//     let query = []
+//     for(let i = 0; i < queries.length; i++) {
+//         for(let j = 0; j < queries[i].length; j++) {
+//           query.push(queries[i][j])
+//         }
+//     }
+//     // console.log(query)
+//      for(let k = 0; k < arr.length; k++) {
+//                    query.map((v) => k === v ? result.push(arr[k] + 1) : result.push(arr[k]))
+                  
+//                 }
+//   return result
+// }
+    // 1. queries를 순환하면서 배열 원소를 꺼낸다
+    // 2. arr에 queries의 index위치를 찾아서 각각 1씩 더한다
+    // 3. 각각 누적해서 최종 arr를 구한다.
