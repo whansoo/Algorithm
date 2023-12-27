470217c69b0e3b95e041cf7d15beea08034ed661
신환수(모두의희망) <ghkstn7523@naver.com>
2023-12-27 14:12:44 +0900

    [level unrated] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -1,7 +1,8 @@
 function solution(num_list, n) {
-    let result = []
-    for(let i = 0; i <= n - 1; i++) {
-        result.push(num_list[i])
-    }
-    return result
+    // let result = []
+    // for(let i = 0; i <= n - 1; i++) {
+    //     result.push(num_list[i])
+    // }
+    // return result
+    return num_list.slice(0,n)
 }
