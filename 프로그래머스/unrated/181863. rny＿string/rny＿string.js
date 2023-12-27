93a6797d7fa907eff750ca1d613c40c08fa8c949
신환수(모두의희망) <ghkstn7523@naver.com>
2023-12-27 19:20:01 +0900

    [level unrated] Title: rny_string, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.js
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.js
@@ -1,13 +1,13 @@
 function solution(rny_string) {
-    let result = ''
-    for(let i = 0; i < rny_string.length; i++) {
-        if(rny_string[i] === 'm') {
-            // rny_string[i] = 'rn'
-            result += 'rn'
-        } else {
-            result += rny_string[i]
-        }
+//     let result = ''
+//     for(let i = 0; i < rny_string.length; i++) {
+//         if(rny_string[i] === 'm') {
+//             result += 'rn'
+//         } else {
+//             result += rny_string[i]
+//         }
         
-    }
-    return result
+//     }
+//     return result
+    return rny_string.replaceAll('m','rn')
 }
