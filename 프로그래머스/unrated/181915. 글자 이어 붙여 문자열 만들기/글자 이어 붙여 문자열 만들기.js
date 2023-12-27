b53340c08445ebcfacc6be1b661aeeeadac3409a
신환수(모두의희망) <ghkstn7523@naver.com>
2023-12-27 15:36:06 +0900

    [level unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,5 +1,9 @@
 function solution(my_string, index_list) {
-    const arr = [...my_string]
-  return index_list.map(v => arr[v]).join('')
-  
+  //   const arr = [...my_string]
+  // return index_list.map(v => arr[v]).join('')
+    let result = []
+    for(let i = 0; i < index_list.length; i++) {
+        result.push(my_string[index_list[i]])
+    }
+ return result.join('')
 }
