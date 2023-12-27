ebc37c36ebc334c9257f6e2fd04a3752de558bbb
신환수(모두의희망) <ghkstn7523@naver.com>
2023-12-27 16:59:53 +0900

    [level unrated] Title: 원하는 문자열 찾기, Time: 0.14 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,11 +1,12 @@
 function solution(myString, pat) {
-   const my = myString.toLowerCase()
-   const pastr = pat.toLowerCase()
+//    const my = myString.toLowerCase()
+//    const pastr = pat.toLowerCase()
    
-   if(my.includes(pastr)) {
-       return 1
-   } else {
-       return 0
-   }
-   console.log(my.includes(pastr))
+//    if(my.includes(pastr)) {
+//        return 1
+//    } else {
+//        return 0
+//    }
+//    console.log(my.includes(pastr))
+    return myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0
 }
