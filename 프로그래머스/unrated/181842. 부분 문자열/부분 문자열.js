c4127b9d6bfa03b604f5f77474b604e53996c94d
신환수(모두의희망) <ghkstn7523@naver.com>
2023-12-27 14:24:38 +0900

    [level unrated] Title: 부분 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.js
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.js
@@ -1,4 +1,6 @@
 function solution(str1, str2) {
-    let arr = [str2]
-   return Number(arr.map(a => a.includes(str1) ? 1 : 0).join())
+   //  let arr = [str2]
+   // const result = arr.map(a => a.includes(str1) ? 1 : 0)
+   //  return Number(result)
+    return str2.includes(str1) ? 1 : 0
 }
