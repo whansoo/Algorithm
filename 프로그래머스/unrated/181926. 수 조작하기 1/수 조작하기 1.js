12d70fd05a9e083a3392633bc0349437130ec86a
신환수(모두의희망) <ghkstn7523@naver.com>
2023-12-27 15:52:28 +0900

    [level unrated] Title: 수 조작하기 1, Time: 3.76 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,27 +1,35 @@
+// function solution(n, control) {
+//     let result = n;
+//     const newctr = [...control]
+//     const arr = ['w', 's', 'd', 'a']
+//     for(let i = 0; i < newctr.length; i++) {
+//          for(let j = 0; j < arr.length; j++) {
+//            if(newctr[i] === arr[j] && arr[j] === 'w') {
+//                result += 1
+//            } else if(newctr[i] === arr[j] && arr[j] === 's') {
+//                result -= 1
+//            }else if(newctr[i] === arr[j] && arr[j] === 'd') {
+//                result += 10
+//            } else if(newctr[i] === arr[j] && arr[j] === 'a') {
+//                result -= 10
+//            }
+//         }
+//     }
+//     return result
+// }
 function solution(n, control) {
     let result = n;
-    // const obj = {
-    //     'w' : 1,
-    //     's' : -1,
-    //     'd' : 10,
-    //     'a' : -10
-    // }
-    const newctr = [...control]
-    const arr = ['w', 's', 'd', 'a']
-    for(let i = 0; i < newctr.length; i++) {
-         for(let j = 0; j < arr.length; j++) {
-           if(newctr[i] === arr[j] && arr[j] === 'w') {
-               result += 1
-           } else if(newctr[i] === arr[j] && arr[j] === 's') {
-               result -= 1
-           }else if(newctr[i] === arr[j] && arr[j] === 'd') {
-               result += 10
-           } else if(newctr[i] === arr[j] && arr[j] === 'a') {
-               result -= 10
-           }
-        }
-        // console.log(newctr[i] === Object.keys(obj)[i])
-    }
-    // console.log(Object.keys(obj)[0])
+     for(let i = 0; i < control.length; i++) {
+         if(control[i] === 'w') {
+             result += 1
+         }else if(control[i] === 's') {
+             result -= 1
+         }else if(control[i] === 'd') {
+             result += 10
+         }else if(control[i] === 'a') {
+             result -= 10
+         }
+     }
+       
     return result
 }
